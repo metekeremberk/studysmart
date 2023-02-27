da82fc61a940fb052af241af84c2384230487888
metekeremberk <metekeremberk@gmail.com>
2023-02-27 17:46:34 +0300

    Landing page ready

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<div
+	class="sticky w-screen h-[10vh] bg-white flex flex-row items-center justify-between z-50 backdrop-filter backdrop-blur-3xl"
+>
+	<h1 class="text-3xl font-bold px-10">StudySmart</h1>
+	<div class="mx-20 text-2xl grid grid-cols-5 gap-10">
+		<a href="/">Link</a>
+		<a href="/">Link</a>
+		<a href="/">Link</a>
+		<a href="/">Link</a>
+		<a href="/">Link</a>
+	</div>
+</div>
+
+<slot />
